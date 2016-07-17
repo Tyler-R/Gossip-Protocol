2a2cf09c5efe27926ffc6bccf8dad7d0ab79dcd8
Tyler Rubenuik <trubenui@sfu.ca>
2016-07-17 03:03:31 -0700

    Added timeout to main for easier debugging

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,26 @@ public class Main {
 	public static void main(String[] args) {
 		Gossip gossip = new Gossip("127.0.0.1", 8081, "192.168.0.188", 8080);
 		Gossip g = new Gossip("127.0.0.1", 8080, "192.168.0.188", 8081);
-		Gossip gossip2 = new Gossip("127.0.0.1", 8082, "192.168.0.188", 8081);
-		Gossip gossip3 = new Gossip("127.0.0.1", 8083, "192.168.0.188", 8081);
+//		Gossip gossip2 = new Gossip("127.0.0.1", 8082, "192.168.0.188", 8081);
+//		Gossip gossip3 = new Gossip("127.0.0.1", 8083, "192.168.0.188", 8081);
 		
 		gossip.start();
 		g.start();
-		gossip2.start();
-		gossip3.start();
+//		gossip2.start();
+//		gossip3.start();
+		
+		new Thread(()-> {
+			try {
+				Thread.sleep(5000);
+				gossip.stop();
+				g.stop();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("exiting program");
+			
+			System.exit(10);
+		}).start();;
 	}
 }
