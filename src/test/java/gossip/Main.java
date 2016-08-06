42a3c4fb617d9503749bfe68b0de2d4c8e7d8376
Tyler-R <trubenui@sfu.ca>
2016-08-06 02:02:27 -0700

    update main class to use config class

--- src/test/java/gossip/Main.java
+++ src/test/java/gossip/Main.java
@@ -1,10 +1,13 @@
 package test.java.gossip;
+import main.java.gossip.Config;
 import main.java.gossip.Gossip;
 
 public class Main {
 	public static void main(String[] args) {
-		Gossip gossip = new Gossip("127.0.0.1", 8081, "127.0.0.1", 8080);
-		Gossip g = new Gossip("127.0.0.1", 8080, "127.0.0.1", 8081);
+		Config config = new Config(2, 2, 3, 500, 200);
+		
+		Gossip gossip = new Gossip("127.0.0.1", 8081, "127.0.0.1", 8080, config);
+		Gossip g = new Gossip("127.0.0.1", 8080, "127.0.0.1", 8081, config);
 //		Gossip gossip2 = new Gossip("127.0.0.1", 8082, "192.168.0.188", 8081);
 //		Gossip gossip3 = new Gossip("127.0.0.1", 8083, "192.168.0.188", 8081);
 		
