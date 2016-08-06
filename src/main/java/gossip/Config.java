7badd97d9c55d90854fd189e3a66e7278daead50
Tyler-R <trubenui@sfu.ca>
2016-08-06 01:20:11 -0700

    Add configuration class to store configrable values

---
+++ src/main/java/gossip/Config.java
@@ -0,0 +1,24 @@
+package main.java.gossip;
+
+public class Config {
+	
+	// Values for members class to use
+	public final int MEMBER_FAILURE_TIMEOUT;
+	public final int MEMBER_CLEANUP_TIMEOUT;
+	
+	// Values for Gossip class to use
+	public final int PEERS_TO_UPDATE_PER_INTERVAL; 
+	public final int UPDATE_FREQUENCY;
+	public final int FAILURE_DETECTION_FREQUENCY;
+	
+	public Config(int memberFailureTimeout, int memberCleanupTimeout, 
+			int peersToUpdatePerInterval, int UpdateFrequency, int failureDetectionFrequency) {
+		MEMBER_FAILURE_TIMEOUT = memberFailureTimeout;
+		MEMBER_CLEANUP_TIMEOUT = memberCleanupTimeout;
+		
+		PEERS_TO_UPDATE_PER_INTERVAL = peersToUpdatePerInterval;
+		UPDATE_FREQUENCY = UpdateFrequency;
+		FAILURE_DETECTION_FREQUENCY = failureDetectionFrequency;
+		
+	}
+}
