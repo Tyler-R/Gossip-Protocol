07561969783cf154cdd5d8383f483f749980b3da
Tyler Rubenuik <trubenui@sfu.ca>
2016-08-26 00:04:17 -0700

    Changed config duration values to use duration class

--- src/main/java/gossip/Config.java
+++ src/main/java/gossip/Config.java
@@ -1,20 +1,21 @@
 package main.java.gossip;
 
 import java.io.Serializable;
+import java.time.Duration;
 
 public class Config implements Serializable {
 	
 	// Values for members class to use
-	public final int MEMBER_FAILURE_TIMEOUT;
-	public final int MEMBER_CLEANUP_TIMEOUT;
+	public final Duration MEMBER_FAILURE_TIMEOUT;
+	public final Duration MEMBER_CLEANUP_TIMEOUT;
 	
 	// Values for Gossip class to use
 	public final int PEERS_TO_UPDATE_PER_INTERVAL; 
-	public final int UPDATE_FREQUENCY;
-	public final int FAILURE_DETECTION_FREQUENCY;
+	public final Duration UPDATE_FREQUENCY;
+	public final Duration FAILURE_DETECTION_FREQUENCY;
 	
-	public Config(int memberFailureTimeout, int memberCleanupTimeout, 
-			int peersToUpdatePerInterval, int UpdateFrequency, int failureDetectionFrequency) {
+	public Config(Duration memberFailureTimeout, Duration memberCleanupTimeout, 
+			Duration UpdateFrequency, Duration failureDetectionFrequency, int peersToUpdatePerInterval) {
 		MEMBER_FAILURE_TIMEOUT = memberFailureTimeout;
 		MEMBER_CLEANUP_TIMEOUT = memberCleanupTimeout;
 		
