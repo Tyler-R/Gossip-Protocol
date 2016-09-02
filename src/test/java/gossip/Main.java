88a6b25238ceb66733d872b0125a6df37337ad56
Tyler Rubenuik <trubenui@sfu.ca>
2016-09-02 00:55:26 -0700

    Add example of logger in main class

--- src/test/java/gossip/Main.java
+++ src/test/java/gossip/Main.java
@@ -6,8 +6,12 @@ import main.java.gossip.Config;
 import main.java.gossip.Gossip;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Config config = new Config(Duration.ofSeconds(2), Duration.ofSeconds(2), Duration.ofMillis(500), Duration.ofMillis(200), 3);
+		
+		Gossip.setLogger((message) -> {
+			System.out.println("Gossip Error: " + message);
+		});
 		
 		Gossip gossip = new Gossip(new InetSocketAddress("127.0.0.1", 8081), new InetSocketAddress("127.0.0.1", 8080), config);
 		Gossip g = new Gossip(new InetSocketAddress("127.0.0.1", 8080), new InetSocketAddress("127.0.0.1", 8081), config);
